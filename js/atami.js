f0596b06f4f17ecd3e16e85cd4f9d0e5696618af
s1f102502306 <s1F102502306@iniad.org>
2025-06-27 17:27:56 +0900

    Create atami.css ＆ atami.js

---
+++ js/atami.js
@@ -0,0 +1,8 @@
+// スクロールでヘッダー固定＆フェード処理
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    document.body.classList.add('scrolled');
+  } else {
+    document.body.classList.remove('scrolled');
+  }
+});
