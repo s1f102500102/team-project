a09f0511d9bcd055843867750c4e31c049f2b34a
s1f102500102 <s1f102500102@iniad.org>
2025-06-27 16:44:53 +0900

    Sumpleなどを追加
    Sumpleに従って

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// ヒーローのフェード＆ヘッダー固定トリガー
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    document.body.classList.add('scrolled');
+  } else {
+    document.body.classList.remove('scrolled');
+  }
+});
